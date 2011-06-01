b0307479f1f1be78b2f3cfd1349d00b5865b1c35
Bernhard Haslhofer <bernhard.haslhofer@cornell.edu>
2011-06-01 16:43:25 -0400

    finished NT links

--- lib/ese2edm/conf/datasets.rb
+++ lib/ese2edm/conf/datasets.rb
@@ -9,7 +9,12 @@ module ESE2EDM
     # A wrapper around the void dataset description for convenient access
     class Dataset
       
-      DEFAULT_DATASETS_PATH = "dataset"
+      DEFAULT_DOWLNLOAD_BASE_DIR = "http://data.europeana.eu/download"
+      
+      DEFAULT_VERSION = 1.0
+      
+      DEFAULT_DATASETS_PATH = "datasets"
+      
       DEFAULT_LINKS_PATH = "links"
       
       attr_reader :uri
@@ -36,7 +41,34 @@ module ESE2EDM
         return xml_files
       end
       
-      # returns all subsets of this dataset (recursively if desired)
+      # returns whether or not downloadable files are associated with that dataset
+      def files?
+        not xml_files.empty?
+      end
+      
+      # the returns the NT-file URIs associated with this dataset
+      def nt_files
+        nt_files = []
+        xml_files.each do |xml_file|
+          basename = File.basename(xml_file, ".xml")
+          nt_files << dataset_baseURI + "/nt/" + basename + ".nt"
+        end
+        return nt_files
+      end
+      
+      # returns all subsets of this dataset
+      def subsets
+        subsets = []
+        each_subset(false) {|subset| subsets << subset}
+        return subsets
+      end
+      
+      # returns whether or not this dataset has subsets
+      def subsets?
+        not subsets.empty?
+      end
+      
+      # iterates through all subsets of this dataset (recursively if desired)
       def each_subset(recursive = false)
         query_property_values :predicate => VOID.subset do |object|
           subset = Dataset.new :graph => @graph, :uri => object
@@ -45,29 +77,65 @@ module ESE2EDM
         end
       end
       
-      # generates a html version 
-      def to_html
-        builder = Nokogiri::HTML::Builder.new do |doc|
-          doc.html {
-            doc.body {
-              doc.ul {
-                doc.li "Bla"
-                doc.li "Blub"
-              }
-            }
-          }
-        end
-        html = builder.to_html
+      # returns the base URI of the dataset download directory
+      def dataset_baseURI
+        DEFAULT_DOWLNLOAD_BASE_DIR + "/" + DEFAULT_VERSION.to_s + "/" + DEFAULT_DATASETS_PATH
+      end
+
+      # returns the base URI of the dataset download directory
+      def links_baseURI
+        DEFAULT_DOWLNLOAD_BASE_DIR + "/" + DEFAULT_VERSION + "/" + DEFAULT_LINKS_PATH
+      end
+      
+      # outputs datset info as HTML (recursively, if desired)
+      def to_html(recursive = false)
         
-        doc = REXML::Document.new(html)
-        doc.write($stdout, indent_spaces = 4)
-        
+        tree = html_tree
+        doc = Nokogiri::HTML tree
+
+      end
+      
+      # the dataset title is the default name 
+      def to_s
+        title
       end
       
       # loads the void dataset from a given file
       def self.load(file, uri)
         graph = RDF::Graph.load(file)
         Dataset.new :graph => graph, :uri => uri
+      end
+
+    protected
+      
+      # outputs information on this dataset as HTML list
+      def html_tree
+        
+        list = <<-EOHTML
+        <ul>
+          <li>
+            <p>#{title}</p>
+        EOHTML
+        
+        if files?
+          list << "<p>"
+          nt_files.each do |nt_file|
+            list << <<-EOHTML 
+              <a href="#{nt_file}">nt</a>
+          EOHTML
+          end
+          list << "</p>"
+        end
+        
+        if subsets?
+          subsets.each do |subset|
+            list << subset.html_tree
+          end
+        end
+
+        list << "</li>"
+        list << "</ul>"
+
       end
 
     private
@@ -108,14 +176,14 @@ module ESE2EDM
     
 end 
 
-
 dataset = ESE2EDM::Dataset.load("../../../datasets/edm-datasets-1.0.ttl", "http://data.europeana.eu/void.ttl#EuropeanaLOD")
 
-dataset.each_subset(true) do |dataset|
-  puts dataset.uri
-  puts dataset.title
-  #puts dataset.xml_files
-  dataset.xml_files.each {|xml_file| puts xml_file}
-end
+p dataset.subsets
 
-#dataset.to_html
+# dataset.each_subset(true) do |dataset|
+#   puts dataset.uri
+#   puts dataset.title
+#   dataset.xml_files.each {|xml_file| puts xml_file}
+# end
+
+File.open("test.html", "w") {|file| file.puts dataset.to_html}
