bccf939a71bddcadaf98476ca34a91b2efee4348
Bernhard Haslhofer <bernhard.haslhofer@cornell.edu>
2011-06-01 10:54:44 -0400

    implemented dataset paring

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :rubygems
+
+# required for generating HTML dataset descriptions
+gem "nokogiri", ">=1.4.4"
+
+# required for parsing RDF dataset description
+gem "rdf", ">=0.3.3"
+gem "rdf-raptor", ">=0.4.1"
--- lib/ese2edm/conf/datasets.rb
+++ lib/ese2edm/conf/datasets.rb
@@ -1,5 +1,8 @@
 require 'rdf'
-require 'rdf/raptor' 
+require 'rdf/raptor'
+
+require 'nokogiri'
+require 'rexml/document'
 
 module ESE2EDM  
     
@@ -17,22 +20,47 @@ module ESE2EDM
         @uri = args[:uri]
       end
       
-      # returns all XML file URIs associated with a data file
-      def each_xml_file
-        
-        query_property_values :subject => @uri, :predicate => ESE2EDM::EULOD.xmlfile do |xml_file|
-          yield xml_file
+      # returns the dataset title
+      def title
+        query_property_values :predicate => DCTERMS.title do |title|
+          return title
         end
-        
       end
       
-      # returns all subsets of this dataset 
-      def each_subset
-        
-        query_property_values :subject => @uri, :predicate => ESE2EDM::VOID.subset do |object|
-          dataset = Dataset.new :graph => @graph, :uri => object
-          yield dataset
+      # returns all XML file URIs associated with a data file
+      def xml_files
+        xml_files = []
+        query_property_values :predicate => EULOD.xmlfile do |xml_file|
+          xml_files << xml_file
         end
+        return xml_files
+      end
+      
+      # returns all subsets of this dataset (recursively if desired)
+      def each_subset(recursive = false)
+        query_property_values :predicate => VOID.subset do |object|
+          subset = Dataset.new :graph => @graph, :uri => object
+          yield subset
+          subset.each_subset(recursive) {|subset| yield subset} if recursive
+        end
+      end
+      
+      # generates a html version 
+      def to_html
+        builder = Nokogiri::HTML::Builder.new do |doc|
+          doc.html {
+            doc.body {
+              doc.ul {
+                doc.li "Bla"
+                doc.li "Blub"
+              }
+            }
+          }
+        end
+        html = builder.to_html
+        
+        doc = REXML::Document.new(html)
+        doc.write($stdout, indent_spaces = 4)
         
       end
       
@@ -44,9 +72,8 @@ module ESE2EDM
 
     private
       
-      # yields property values for a given subject and predicate
+      # yields property values for the URI subject and a given property
       def query_property_values(args)
-        subject = args[:subject]
         predicate = args[:predicate]
         
         # retrieve all matching triples
@@ -55,29 +82,40 @@ module ESE2EDM
         end
         
         # filter the one having the dataset uri as subject
-        solutions.filter(:subject  => RDF::URI(subject)).each do |solution|
-          object = solution[:object]
+        solutions.filter(:subject  => RDF::URI(@uri)).each do |solution|
+          object = solution[:object].to_s
           yield object
         end
       end
       
-    end
+    end # end of class dataset
+    
     
     # Europeana-LOD specific terms used in void description
     class EULOD < RDF::Vocabulary("http://data.europeana.eu/conf#")
       property :xmlfile
     end
     
+    # VOID vocabulary
     class VOID < RDF::Vocabulary("http://rdfs.org/ns/void#")
       property :subset
+    end
+    
+    # DCTerms vocabulary
+    class DCTERMS < RDF::Vocabulary("http://purl.org/dc/terms/")
+      property :title
     end
     
 end 
 
 
-dataset = ESE2EDM::Dataset.load("../../../datasets/edm-datasets-1.0.ttl", "http://data.europeana.eu/void.ttl#TEL")
+dataset = ESE2EDM::Dataset.load("../../../datasets/edm-datasets-1.0.ttl", "http://data.europeana.eu/void.ttl#EuropeanaLOD")
 
-dataset.each_subset do |dataset|
+dataset.each_subset(true) do |dataset|
   puts dataset.uri
-  dataset.each_xml_file {|xml_file| puts xml_file}
+  puts dataset.title
+  #puts dataset.xml_files
+  dataset.xml_files.each {|xml_file| puts xml_file}
 end
+
+#dataset.to_html
